0492897e36051da87e6707d66e6156efe03000d8
so_young <qksthdhd94@gmail.com>
2019-11-05 22:20:06 +0900

    보조 생성자를 이용한 코드 생성

--- app/src/main/java/com/example/a191105_01_listview/datas/PostData.kt
+++ app/src/main/java/com/example/a191105_01_listview/datas/PostData.kt
@@ -1,8 +1,13 @@
 package com.example.a191105_01_listview.datas
 
+import android.util.Log
+
 class PostData(inputTitle:String, inputContent:String) {
 
     var title = inputTitle
     var content = inputContent
 
+    init{
+        Log.d("게시글 생성","기본 생성자로 생성")
+    }
 }
